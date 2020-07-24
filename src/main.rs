b47a647605df8018cef3c50ada6cbfad02525b7a
Vinícius Regatieri <viniciusvcr@hotmail.com>
2020-07-23 21:04:18 -0300

    implemented pre-lexer

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,35 @@ mod token;
 
 use error::Error;
 
-fn run(source_code: String) {
-    println!("{}", source_code);
+fn create_code_vec(source_code: &str) -> std::vec::Vec<&str> {
+    let source_code_len = source_code.len();
+    let mut current_char = 0;
+    let mut line_start = 0;
+
+    if source_code_len > 0 {
+        let mut vec_lines: std::vec::Vec<&str> = vec![];
+        while current_char < source_code_len {
+            if let Some(c) = source_code.chars().nth(current_char) {
+                if c == '\n' {
+                    vec_lines.push(&source_code[line_start..current_char]);
+                    line_start = current_char + 1;
+                }
+            }
+            current_char += 1;
+        }
+        if let Some(c) = source_code.chars().nth(source_code_len - 1) {
+            if c != '\n' {
+                vec_lines.push(&source_code[line_start..current_char])
+            }
+        }
+        vec_lines
+    } else {
+        vec![]
+    }
+}
+
+fn run(source_code: &str) {
+    println!("{:?}", create_code_vec(source_code));
 }
 
 fn main() {
@@ -15,14 +42,7 @@ fn main() {
     if argv_len == 2 {
         if let Some(path) = argv.nth(1) {
             if let Ok(source_code) = std::fs::read_to_string(path) {
-                run(source_code);
-            } else {
-                let error = Error::Input(
-                    "Something went wrong while reading the file".to_string(),
-                    "Verify the path provided and try again".to_string(),
-                );
-                error.show_error(None);
-                exit(64);
+                run(&source_code);
             }
         } else {
             let error = Error::Input(
@@ -47,4 +67,47 @@ fn main() {
         error.show_error(None);
         exit(65);
     }
+}
+
+#[test]
+fn create_code_vec_hello_world() {
+    let hello_word = "print(\"Hello, world!\")";
+
+    assert_eq!(create_code_vec(hello_word), ["print(\"Hello, world!\")"]);
+}
+
+#[test]
+fn create_code_vec_three_lines() {
+    let three_lines = "print(\"Hello, world!\")\n2 + 2\n2";
+
+    assert_eq!(
+        create_code_vec(three_lines),
+        ["print(\"Hello, world!\")", "2 + 2", "2"]
+    );
+}
+
+#[test]
+fn create_code_vec_three_lines_with_empty_line() {
+    let three_lines = "print(\"Hello, world!\")\n2 + 2\n2\n\n";
+
+    assert_eq!(
+        create_code_vec(three_lines),
+        ["print(\"Hello, world!\")", "2 + 2", "2", ""]
+    );
+}
+
+#[test]
+fn create_code_vec_three_lines_with_endline() {
+    let three_lines = "print(\"Hello, world!\")\n2 + 2\n2\n";
+
+    assert_eq!(
+        create_code_vec(three_lines),
+        ["print(\"Hello, world!\")", "2 + 2", "2"]
+    );
+}
+
+#[test]
+fn create_code_vec_empty() {
+    let empty_vec: std::vec::Vec<&str> = vec![];
+    assert_eq!(create_code_vec(""), empty_vec);
 }
